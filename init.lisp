528ddd9e204d2ba82dc14868c8af182352c9815a
John Process <esologic@gmail.com>
2011-04-11 22:39:00 -0400

    k means clustering alg

--- init.lisp
+++ init.lisp
@@ -1,9 +1,9 @@
 (require 'asdf)
 
-;(pushnew :pcall *features*)
-;(load "~/quicklisp/setup.lisp")
-;(ql:quickload "pcall")
+(pushnew :pcall *features*)
+(load "~/quicklisp/setup.lisp")
+(ql:quickload "pcall")
 
 (asdf:oos 'asdf:load-op 'elray)
 
-(elray:trace-to-file "test.pgm")
+;;(elray:trace-to-file "test.pgm")
